7ff34aff66fcb50eb71616b051f6010d135a1844
sbhta <sbhta12345@gmail.com>
2024-12-27 17:03:55 +0200

    Fixed day 2

--- 02/src/main.cpp
+++ 02/src/main.cpp
@@ -4,106 +4,58 @@
 #include <string>
 #include <vector>
 
+// check if a level is safe
+bool isReportSafe(std::vector<int> report){
+   // check if ascending or decending
+   bool ascending = report[0]-report[1] < 0;
+   for (int i = 0; i < report.size()-1; ++i){
+      if (report[i] - report[i+1] == 0) {return false;}
+      if (ascending && report[i] - report[i+1] > 0){return false;}
+      if (!ascending && report[i] - report[i+1] < 0){return false;}
+      if (std::abs(report[i]-report[i+1]) > 3) {return false;}
+   }
+   return true;
+}
 int part1(std::vector<std::string> inp){
-   int count = 0;
-   
+   // compile the input into reports
    std::vector<std::vector<int>> reports;
-
    for (std::string s : inp){
       std::string nS = "";
       std::vector<int> rp;
       for (char ch : s){
-         if (ch == ' '){
-            rp.push_back(std::stoi(nS));
-            nS = "";
-         }
-         else {
-            nS += ch;
-         }
+         if (ch == ' '){ rp.push_back(std::stoi(nS)); nS = ""; }
+         else nS += ch; 
       }
       rp.push_back(std::stoi(nS));
       reports.push_back(rp);
    }
+   int count = 0;
    for (std::vector<int> repo : reports){
-      // check if ascending or decending
-      bool ascending = repo[0]-repo[1] < 0;
-      bool safe = true;
-      if (repo[0] - repo[1] == 0) continue;
-      for (int i = 0; i < repo.size()-1; ++i){
-         if (repo[i] - repo[i+1] == 0) {safe = false;break;}
-         if (ascending && repo[i] - repo[i+1] > 0){safe = false; break;}
-         if (!ascending && repo[i] - repo[i+1] < 0){safe = false; break;}
-         if (std::abs(repo[i]-repo[i+1]) > 3) {safe = false; break;}
-      }
-      if (safe) count++;
+      if (isReportSafe(repo)) count++;
    }
    return count;
 }
-
-// a function that gives all of the reports possible when removing one of the levels
-std::vector<std::vector<int>> removeOneLevel(std::vector<int> repo){
-   std::vector<std::vector<int>> perms;
-   std::vector<int> tempRepo;
-   for (int skip = 0; skip < repo.size(); ++skip){
-      for (int i = 0; i < repo.size(); ++i){
-         if (skip != i)tempRepo.push_back(repo[i]);
-      }
-      perms.push_back(tempRepo);
-      tempRepo.clear();
-   }
-
-   return perms;
-}
-
 int part2(std::vector<std::string> inp){
-   int count = 0;
-   
+   // compile the input into reports
    std::vector<std::vector<int>> reports;
-
    for (std::string s : inp){
       std::string nS = "";
       std::vector<int> rp;
       for (char ch : s){
-         if (ch == ' '){
-            rp.push_back(std::stoi(nS));
-            nS = "";
-         }
-         else {
-            nS += ch;
-         }
+         if (ch == ' '){ rp.push_back(std::stoi(nS)); nS = ""; }
+         else nS += ch; 
       }
       rp.push_back(std::stoi(nS));
       reports.push_back(rp);
    }
+   int count = 0;
    for (std::vector<int> repo : reports){
-      // check if ascending or decending
-      bool ascending = repo[0]-repo[1] < 0;
-      bool safe = true;
-      if (repo[0] - repo[1] == 0) continue;
-      for (int i = 0; i < repo.size()-1; ++i){
-         if (repo[i] - repo[i+1] == 0) {safe = false;break;}
-         if (ascending && repo[i] - repo[i+1] > 0){safe = false; break;}
-         if (!ascending && repo[i] - repo[i+1] < 0){safe = false; break;}
-         if (std::abs(repo[i]-repo[i+1]) > 3) {safe = false; break;}
-      }
-
-      if (safe) count++;
+      if (isReportSafe(repo)) count++;
       else {
-         std::vector<std::vector<int>> perms = removeOneLevel(repo);
-         for (std::vector<int> rp : perms){
-            bool ascending2 = rp[0]-rp[1] < 0;
-            bool safe2 = true;
-            if (rp[0] - rp[1] == 0) continue;
-            for (int i = 0; i < rp.size()-1; ++i){
-               if (rp[i] - rp[i+1] == 0) {safe2 = false;break;}
-               if (ascending2 && rp[i] - rp[i+1] > 0){safe2 = false; break;}
-               if (!ascending2 && rp[i] - rp[i+1] < 0){safe2 = false; break;}
-               if (std::abs(rp[i]-rp[i+1]) > 3) {safe2 = false; break;}
-            }
-            if (safe2) {
-               count++;
-               break;
-            }
+         for (int i = 0; i < repo.size(); ++i){
+            std::vector<int> cRepo = repo;
+            cRepo.erase(cRepo.begin()+i);
+            if (isReportSafe(cRepo)) {count++; break;}
          }
       }
    }
@@ -130,6 +82,6 @@ int main (int argc, char *argv[]) {
    }
    file.close();
    std::cout << "Part 1: " << part1(input) << std::endl;
-   std::cout << "NOT CORRECT YET Part 2: " << part2(input) << std::endl;
+   std::cout << "Part 2: " << part2(input) << std::endl;
    return 0;
 }
