f55f2f0af8e23a7ad213058066adb14b4efe18d3
sbhta <sbhta12345@gmail.com>
2024-12-28 01:00:15 +0200

    Fixed day 11
    Took me waaaaaay to long to realize that both parts returned an int and
    that the answer to part 2 was a long

--- 11/src/main.cpp
+++ 11/src/main.cpp
@@ -1,73 +1,114 @@
 #include <fstream>
 #include <iostream>
-#include <list>
 #include <map>
 #include <string>
-#include <utility>
+#include <tuple>
 #include <vector>
 #include <cmath>
-int getLength(long long number) {
-    return static_cast<int>(std::log10(std::abs(number))) + 1;
-}
-std::pair<long long, long long> splitNumber(long long number) {
-    int length = static_cast<int>(std::log10(number)) + 1;
-    int halfLength = length / 2;
-    long long divisor = static_cast<long long>(std::pow(10, halfLength));
+std::vector<long long> splitNumber(long long number) {
+    // Count the digits
+    int numDigits = static_cast<int>(std::log10(number)) + 1;
 
+    // Calculate the divisor
+    long long divisor = std::pow(10, numDigits / 2);
+
+    // Split the number
     long long firstHalf = number / divisor;
     long long secondHalf = number % divisor;
 
     return {firstHalf, secondHalf};
 }
-
-std::map<long long, long long> mapSingle;
-std::map<long long, std::pair<long long, long long>> mapDoubles;
-
-void changeStones(std::vector<long long>& stones){
-   for (int i = 0; i < stones.size(); ++i){
-      if (stones[i] == 0) stones[i] = 1;
-      else if (getLength(stones[i])%2 == 0){
-         if (mapDoubles.find(stones[i]) != mapDoubles.end()){
-            stones.insert(stones.begin()+i+1, mapDoubles[stones[i]].second);
-            stones[i] = mapDoubles[stones[i]].first;
-            i++;
-         }
-         else {
-            mapDoubles[stones[i]] = splitNumber(stones[i]);
-            stones.insert(stones.begin()+i+1, mapDoubles[stones[i]].second);
-            stones[i] = mapDoubles[stones[i]].first;
-            i++;
-         }
+bool hasEvenNumberOfDigits(long long number) {
+    if (number == 0) {
+        return false; // 0 has 1 digit, which is odd
+    }
+    int numDigits = static_cast<int>(std::log10(number)) + 1;
+    return numDigits % 2 == 0;
+}
+std::vector<unsigned long long> evolveStones(std::vector<unsigned long long> stones){
+   std::vector<unsigned long long> result;
+   for (unsigned long long s : stones){
+      if (s == 0) result.push_back(1);
+      else if (hasEvenNumberOfDigits(s)) {
+         std::vector<long long> twoS = splitNumber(s);
+         result.push_back(twoS[0]);
+         result.push_back(twoS[1]);
       }
       else {
-         if (mapSingle.find(stones[i]) != mapSingle.end()){
-            stones[i] = mapSingle[stones[i]];
-         }
-         else {
-            mapSingle[stones[i]] = stones[i]*2024;
-            stones[i] = mapSingle[stones[i]];
-         }
+         result.push_back(s*2024);
+      }
+   }
+   return result;
+}
+
+// defining the functions in advance
+long long countDescendants(long long num, int iterations);
+long long fullCountDescendants(long long num, int iterations);
+long long countDescendants(std::vector<long long> numbers, int iterations);
+std::vector<long long> expand(long long num);
+
+static std::map<std::tuple<long long, int>, long long> myCache;
+long long countDescendants(std::vector<long long> numbers, int iterations){
+   long long result = 0;
+   for (long long num : numbers){
+      result += countDescendants(num,iterations);
+   }
+   return result;
+}
+long long countDescendants(long long num, int iterations){
+   if (myCache.find({num, iterations}) != myCache.end()){
+      return myCache[{num, iterations}];
+   }
+   else{
+      myCache[{num, iterations}] = fullCountDescendants(num, iterations);
+      return myCache[{num, iterations}];
+   }
+}
+long long fullCountDescendants(long long num, int iterations){
+      if (iterations == 0) return 1;
+      else {return countDescendants(expand(num),iterations-1);}
+}
+std::vector<long long> expand(long long num){
+   if (num == 0) return {1};
+   else {
+      if (hasEvenNumberOfDigits(num)){
+         return splitNumber(num);
+      }
+      else {
+         return {num*2024};
       }
    }
 }
 
-int part1(std::vector<std::string> inp){
-   std::vector<long long> stones;
+long long part1(std::vector<std::string> inp){
+   std::vector<unsigned long long> stones;
    std::string temp = "";
-   for (char ch : inp[0]){
-      if (ch == ' ') {
-         stones.push_back(std::stoll(temp));
+   for (char c : inp[0]){
+      if (c == ' '){
+         stones.push_back(std::stoull(temp));
          temp = "";
       }
-      else { temp += ch; }
+      else temp += c;
    }
-   stones.push_back(std::stoll(temp));
-   for (int i = 0; i < 75; ++i){
-      changeStones(stones);
-      std::cout << i << std::endl;
+   stones.push_back(std::stoull(temp));
+   for (int i = 0; i < 25; ++i){
+      stones = evolveStones(stones);
    }
-
    return stones.size();
+}
+long long part2(std::vector<std::string> inp){
+   std::vector<long long> stones;
+   std::string temp = "";
+   for (char c : inp[0]){
+      if (c == ' '){
+         stones.push_back(std::stoull(temp));
+         temp = "";
+      }
+      else temp += c;
+   }
+   stones.push_back(std::stoull(temp));
+   
+   return countDescendants(stones, 75);
 }
 
 int main (int argc, char *argv[]) {
@@ -89,6 +130,6 @@ int main (int argc, char *argv[]) {
    }
    file.close();
    std::cout << "Part 1: " << part1(input) << std::endl;
-   //std::cout << "Part 2: " << part2(input) << std::endl;
+   std::cout << "Part 2: " << part2(input) << std::endl;
    return 0;
 }
