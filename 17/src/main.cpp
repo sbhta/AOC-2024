a063c5a63fe4ccc9f5dba8515e666bb183908c95
sbhta <sbhta12345@gmail.com>
2024-12-31 14:46:01 +0200

    Fixed day 17 part 1. Part 2 requires more understanding of the language then i currently have

--- 17/src/main.cpp
+++ 17/src/main.cpp
@@ -1,98 +1,121 @@
+#include <cassert>
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <iostream>
-#include <map>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-void adv(int& regA, int operand){ regA = regA/std::pow(2,operand);} //operand is combo
-void bxl(int& regB, int operand){ regB = regB^operand;} // operand is literal
-void bst(int& regB, int operand){ regB = operand%8;} // operand is combo
-bool jnz(int regA, int& pointer, int operand) {if(regA!=0) {pointer=operand;return true;}return false;} //operand is literal
-void bxc(int& regB, int regC){ regB = regB^regC;} // operand is a register
-void bdv(int& regB, int regA, int operand){ regB = regA/std::pow(2,operand);} //operand is combo
-void cdv(int& regC, int regA, int operand){ regC = regA/std::pow(2,operand);} //operand is combo
-
 std::string part1(std::vector<std::string> inp){
-   std::string result = "";
-   int regA = std::stoi(inp[0].substr(12));
-   int regB = std::stoi(inp[1].substr(12));
-   int regC = std::stoi(inp[2].substr(12));
+   int a = std::stoi(inp[0].substr(12));
+   int b = std::stoi(inp[1].substr(12));
+   int c = std::stoi(inp[2].substr(12));
    std::vector<int> program;
-   for (int i = 9; i < inp[4].length(); i+=2){ program.push_back(inp[4][i]-'0'); }
-   int p = 0;
-   while (p <= program.size()){
-      std::map<int, int> combo = {
-         {0, 0},
-         {1, 1},
-         {2, 2},
-         {3, 3},
-         {4, regA},
-         {5, regB},
-         {6, regC},
-         {7, 7},
-      };
-      if (program[p] == 0){adv(regA, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 1){bxl(regB, program[p+1]);p+=2;}
-      else if (program[p] == 2){bst(regB, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 3){ if(!jnz(regA, p, program[p+1]))p+=2;}
-      else if (program[p] == 4){bxc(regB, regC);p+=2;}
-      else if (program[p] == 5){ result += std::to_string(combo[program[p+1]]%8) + ','; p+=2;}
-      else if (program[p] == 6){bdv(regB, regA, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 7){cdv(regC, regA, combo[program[p+1]]);p+=2;}
+   for (int i = 9; i < inp[4].size(); i+=2){ program.push_back(inp[4][i]-'0'); }
+   int pointer = 0;
+   std::vector<int> output;
+   std::function combo = [&a,&b,&c](int op){
+      switch (op) {
+         case 4: return a;break; 
+         case 5: return b;break; 
+         case 6: return c;break; 
+         default: return op;break; 
+      }
+   };
+   while (pointer < program.size()){
+      int ins = program[pointer];
+      int operand = program[pointer+1];
+      switch (ins) {
+         case 0: a = a >> combo(operand);break; // adv
+         case 1: b = b ^ operand;break; // bxl
+         case 2: b = combo(operand)%8;break; // bst
+         case 3: if (a != 0) {pointer = operand; continue;};break; // jnz
+         case 4: b = b ^ c;break; // bxc
+         case 5: output.push_back(combo(operand)%8);break; // out
+         case 6: b = a >> combo(operand);break; // bdv
+         case 7: c = a >> combo(operand);break; // cdv
+      }
+      pointer+=2;
    }
-   std::string actualResult = result.substr(0, result.length()-1);
-   return actualResult;
-}
-std::string part1(std::vector<std::string> inp, int A){
    std::string result = "";
-   std::cout << A << std::endl;
-   int regA = A;
-   int regB = std::stoi(inp[1].substr(12));
-   int regC = std::stoi(inp[2].substr(12));
-   std::vector<int> program;
-   for (int i = 9; i < inp[4].length(); i+=2){ program.push_back(inp[4][i]-'0'); }
-   int p = 0;
-   int run = 0;
-   while (p <= program.size() && run < 10000){
-      run++;
-      std::map<int, int> combo = {
-         {0, 0},
-         {1, 1},
-         {2, 2},
-         {3, 3},
-         {4, regA},
-         {5, regB},
-         {6, regC},
-         {7, 7},
-      };
-      if (program[p] == 0){adv(regA, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 1){bxl(regB, program[p+1]);p+=2;}
-      else if (program[p] == 2){bst(regB, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 3){ if(!jnz(regA, p, program[p+1]))p+=2;}
-      else if (program[p] == 4){bxc(regB, regC);p+=2;}
-      else if (program[p] == 5){ result += std::to_string(combo[program[p+1]]%8) + ','; p+=2;}
-      else if (program[p] == 6){bdv(regB, regA, combo[program[p+1]]);p+=2;}
-      else if (program[p] == 7){cdv(regC, regA, combo[program[p+1]]);p+=2;}
+   for (int i : output){
+      result += std::to_string(i) + ',';
    }
-   std::string actualResult = "";
-   for (char c : result){if(c != ',') actualResult+=c;}
-   return actualResult;
+   result.back() = ' ';
+   return result; 
 }
-int part2(std::vector<std::string> inp){
-   int i = 0;
-   std::string should = "";
-   for (char c : inp[4].substr(9)) {if(c != ',')should+=c;}
-      std::cout << i << std::endl;
-   while (should != part1(inp, i)){
-      std::cout << i << std::endl;
-      i++;
+std::vector<int> program2;
+int find(std::vector<int> target, int ans){
+   std::cout << ans << std::endl;
+   if (target.empty()) return ans;
+   for (int t = 0; t < 8; ++t){
+      int a = ans << 3 | t;
+      int b = 0;
+      int c = 0;
+      int* output = nullptr;
+      bool adv3 = false;
+      std::function combo = [&a,&b,&c](int op){
+         switch (op) {
+            case 4: return a;break; 
+            case 5: return b;break; 
+            case 6: return c;break; 
+            default: return op;break; 
+         }
+      };
+      for (int pointer = 0; pointer < target.size()-2; pointer += 2){
+         int ins = program2[pointer];
+         int operand = program2[pointer+1];
+         if (ins == 0){
+            assert(operand == 3);
+            assert(!adv3);
+            adv3 = true;
+         }
+         else if (ins == 1){
+            b = b ^ operand;
+         }
+         else if (ins == 2){
+            b = combo(operand)%8;
+         }
+         else if (ins == 3){
+            throw std::logic_error("JNZ in body of loop");
+         }
+         else if (ins == 4){
+            b = b ^ c;
+         }
+         else if (ins == 5){
+            assert(output == nullptr);
+            int temp = combo(operand)%8;
+            output = &temp;
+         }
+         else if (ins == 6){
+            b = a >> combo(operand);
+         }
+         else if (ins == 7){
+            c = a >> combo(operand);
+         }
+         if (*output == target.back()){
+            std::vector<int> target2;
+            for (int i = 0; i < target.size()-1; ++i) target2.push_back(target[i]);
+            int sub = find(target2, a);
+            if (sub == -1)continue;
+            return sub;
+         }
+      }
    }
-   return i;
+   return -1;
+}
+long long part2(std::vector<std::string> inp, long long ans){
+   std::vector<int> program;
+   for (int i = 9; i < inp[4].size(); i+=2){ program.push_back(inp[4][i]-'0'); }
+   if (program.empty()) return ans;
+   program2 = program;
+   std::cout << find(program, 2024) << std::endl;
+   return 0;
 }
 
 int main (int argc, char *argv[]) {
-      if (argc < 2) {
+   if (argc < 2) {
       std::cerr << "Error: No file path provided.\n";
       std::cerr << "Usage: " << argv[0] << " <filename>\n";
       return 1;
@@ -110,6 +133,6 @@ int main (int argc, char *argv[]) {
    }
    file.close();
    std::cout << "Part 1: " << part1(input) << std::endl;
-   //std::cout << "Part 2: " << part2(input) << std::endl;
+   std::cout << "Part 2: " << part2(input, 0) << std::endl;
    return 0;
 }
