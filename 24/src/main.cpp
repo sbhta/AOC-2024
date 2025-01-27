be8af44165f68840b3f12e0c4fbbe0a574630f14
sbhta <sbhta12345@gmail.com>
2025-01-27 03:20:25 +0200

    Fixed day 24, Part 2 needs to be generalized and I am not smart enough yet to do so

--- 24/src/main.cpp
+++ 24/src/main.cpp
@@ -5,60 +5,47 @@
 #include <string>
 #include <vector>
 
-struct Gate {
-   std::string output;
-   std::string input1;
-   std::string input2;
-   char op;
-};
-bool check_key(std::map<std::string, bool> m, std::string key)
-{
-    // Key is not present
-    if (m.find(key) == m.end())
-        return false;
-    return true;
+std::map<std::string, bool> known;
+std::map<std::string, std::vector<std::string>> formulas;
+bool calc(std::string wire){
+   if (known.find(wire) != known.end()) return known[wire];
+   std::string op = formulas[wire][0];
+   std::string x = formulas[wire][1];
+   std::string y = formulas[wire][2];
+   if (op == "OR "){ known[wire] = calc(x) | calc(y); }
+   else if (op == "AND"){ known[wire] = calc(x) & calc(y); }
+   else if (op == "XOR"){ known[wire] = calc(x) ^ calc(y); }
+
+   return known[wire];
 }
+
 long long part1(std::vector<std::string> inp){
-   std::map<std::string, bool> wires;
-   size_t start = 0;
-   for (std::string s : inp){
-      start++;
-      if (s == "") break;
-      wires[s.substr(0,3)] = (s[5]=='1');
-   }
-   std::vector<Gate> gates;
-   for (long long i = start; i < inp.size(); ++i){
-      Gate g;
-      switch (inp[i][4]) {
-         case 'X': g.input1=inp[i].substr(0, 3);g.input2=inp[i].substr(8,3);g.op='^';g.output=inp[i].substr(15,3);break;
-         case 'A': g.input1=inp[i].substr(0, 3);g.input2=inp[i].substr(8,3);g.op='&';g.output=inp[i].substr(15,3);break;
-         case 'O': g.input1=inp[i].substr(0, 3);g.input2=inp[i].substr(7,3);g.op='|';g.output=inp[i].substr(14,3);break;
-      }
-      gates.push_back(g);
-   }
-   size_t index = 0;
-   std::vector<size_t> foundValues;
-   while (foundValues.size() < gates.size()){
-      if (std::count(foundValues.begin(), foundValues.end(), index) > 0) {index++;continue;}
-      if (check_key(wires, gates[index].input1)){
-         if (wires.find(gates[index].input2) != wires.end()){
-            foundValues.push_back(index);
-            switch (gates[index].op) {
-               case '^': wires[gates[index].output] = (wires[gates[index].input1]^wires[gates[index].input2]);break;
-               case '&': wires[gates[index].output] = (wires[gates[index].input1]&wires[gates[index].input2]);break;
-               case '|': wires[gates[index].output] = (wires[gates[index].input1]|wires[gates[index].input2]);break;
-            }
-         }
-      }
+   int index = 0;
+   for (std::string s : inp) {
       index++;
-      if (index >= gates.size()) index = 0;
+      if (s == "") break;
+      known[s.substr(0, 3)] = s[5]=='1'?true:false;
    }
-   std::string resultString = "";
-   for (auto [name, value] : wires){ if (name[0] == 'z') resultString+=std::to_string(value); }
-   std::string resultString2 = "";
-   for (long long i = resultString.length()-1; i >= 0; --i){ resultString2 += resultString[i]; }
-   long long result = std::stoll(resultString2, nullptr, 2);
-   return result;
+   for (int i = index;i< inp.size(); ++i){
+      std::string s = inp[i];
+      if (s[4] == 'O') s.insert(7, 1, ' ');
+      std::string x = s.substr(0, 3);
+      std::string op = s.substr(4, 3);
+      std::string y = s.substr(8, 3);
+      std::string z = s.substr(15, 3);
+      formulas[z] = {op, x, y};
+   }
+   for (auto [wire, form] : formulas){
+      calc(wire);
+   }
+   std::string strResult = "";
+   for (auto [wire, value] : known){
+      if (wire[0] != 'z') continue;
+      if (value == true) strResult += '1';
+      else strResult += '0';
+   }
+   std::reverse(strResult.begin(), strResult.end());
+   return std::stoll(strResult, nullptr, 2);
 }
 
 int main (int argc, char *argv[]) {
